016ca62dfa1892410c349c2a4cc1f4534140d8d3
Adrian Popan <adrian.popan@gmail.com>
2019-07-05 11:16:28 -0500

    Test Adrian Popan

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,52 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @all
+  end
+
+# Restaurant#customers
+# Returns a unique list of all customers who have reviewed a particular restaurant.
+
+def customers
+
+end
+
+# Restaurant#reviews
+# returns an array of all reviews for that restaurant
+
+def reviews
+
+end
+
+# Restaurant#average_star_rating
+# returns the average star rating for a restaurant based on its reviews
+
+def average_star_rating
+
+end
+
+# Restaurant#longest_review
+# returns the longest review content for a given restaurant
+
+def longest_review
+
+end
+
+# Restaurant.find_by_name(name)
+# given a string of restaurant name, returns the first restaurant that matches
+
+def self.find_by_name(restaurant_name)
+  @@all.find { |i| i == restaurant_name } 
+end
+
+
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,35 @@
 class Review
-  
+
+    attr_accessor :review, rating
+    attr_reader : author
+    @@all
+
+    def initialize(review, rating)
+        @review = review
+        @author = customer.full_name
+        @@all << self
+    end
+
+    # Review.all
+    # returns all of the reviews
+
+    def self.all
+        @@all
+    end
+
+    # Review#customer
+    # returns the customer object for that given review
+
+    def customer
+
+    end
+    # Once a review is created, I should not be able to change the author
+    # Review#restaurant
+    # returns the restaurant object for that given review
+    # Once a review is created, I should not be able to change the restaurant
+    # Review#rating
+    # returns the star rating for a restaurant. This should be an integer from 1-5
+    # Review#content
+    # returns the review content, as a string, for a particular review
 end
 
